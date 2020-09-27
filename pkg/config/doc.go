42a0c38a98d36de3263451aceae5424854ef47ac
Bjorn Borud <borud@borud.org>
2020-09-28 00:51:01 +0200

    Fixed bug in paths for proto generation

--- pkg/config/doc.go
+++ pkg/config/doc.go
@@ -1,3 +1,3 @@
 // Package config defines the configution protocol
-//go:generate protoc -I=proto/ --go_out=. proto/config.proto
+//go:generate protoc -I=../../proto/ --go_out=. ../../proto/config.proto
 package config
